52a437c90a1806bbfda7db67522023e05634554c
Faiz Ilham <faizilham.faizz@gmail.com>
2021-12-12 18:13:09 +0700

    use site branch in manual deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,9 +4,10 @@ DATE=$(date +"%Y-%m-%d %T")
 
 cd _site
 git init
+git checkout -b site
 git add --all
 git commit -m "Deployment $DATE"
 git remote add origin git@github.com:faizilham/faizilham.github.io.git
-git push -f origin master
+git push -f origin site
 
 echo "Site is successfully deployed"
