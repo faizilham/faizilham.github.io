cd54faf3b7ea68ca1cb4db8d036d7291545cb643
Faiz Ilham <faizilham.faizz@gmail.com>
2025-05-16 16:29:54 +0200

    Update build.sh

--- script/build.sh
+++ script/build.sh
@@ -3,5 +3,6 @@ rm -rf _site
 JEKYLL_ENV=production bundle exec jekyll doctor
 JEKYLL_ENV=production bundle exec jekyll build
 touch _site/.nojekyll
+[ -f CNAME ] && cp CNAME _site/CNAME
 
 echo "Site is successfully built"
